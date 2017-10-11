8f9df3dc0fa25af44e499f756be7293e7fb486af
Aranya Dan <aranya.dan1996@gmail.com>
2017-10-11 21:33:47 +0530

    cleaned junk

--- src/transforms.f90
+++ src/transforms.f90
@@ -2,54 +2,6 @@ module transform
   implicit none
 
 contains
-  function Rinv_mult(q,v,n_x)
-    integer :: n_x,i
-    real,dimension(n_x,3) :: q,q_h,v,Rinv_mult,unew
-    real,dimension(3,3) :: R_inv
-    real,dimension(n_x) :: rho,u,E,p,a
-    real :: temp
-
-    ! q at half steps
-    q_h = u_half(q,n_x)
-    call primitives(q_h,n_x,rho,u,E,p,a)
-
-    ! print*,"condition",u(n_x/2),a(n_x/2)
-    do i=1,n_x
-      R_inv = Rinv(u(i),a(i))
-      ! if(i==n_x/2) then
-      !   print*,R_inv
-      ! end if
-      unew(i,1) = R_inv(1,1) * v(i,1) + R_inv(1,2) * v(i,2) + R_inv(1,3) * v(i,3)
-      unew(i,2) = R_inv(2,1) * v(i,1) + R_inv(2,2) * v(i,2) + R_inv(2,3) * v(i,3)
-      unew(i,3) = R_inv(3,1) * v(i,1) + R_inv(3,2) * v(i,2) + R_inv(3,3) * v(i,3)
-    end do
-    Rinv_mult = unew
-  end function Rinv_mult
-
-
-  function R_mult(q,v,n_x)
-    integer :: n_x,i,j,k
-    real,dimension(n_x,3) :: q,q_h,v,R_mult,unew
-    real,dimension(3,3) :: Rn
-    real,dimension(n_x) :: rho,u,E,p,a
-    real :: ratio,temp
-
-    ! q at half steps
-    q_h = u_half(q,n_x)
-    call primitives(q_h,n_x,rho,u,E,p,a)
-
-    do i=1,n_x
-      Rn = Rcalc(u(i),a(i))
-      ! if(i==n_x/2) then
-      !   print*,Rn
-      ! end if
-      unew(i,1) = Rn(1,1) * v(i,1) + Rn(1,2) * v(i,2) + Rn(1,3) * v(i,3)
-      unew(i,2) = Rn(2,1) * v(i,1) + Rn(2,2) * v(i,2) + Rn(2,3) * v(i,3)
-      unew(i,3) = Rn(3,1) * v(i,1) + Rn(3,2) * v(i,2) + Rn(3,3) * v(i,3)
-    end do
-    R_mult = unew
-  end function R_mult
-
 
   function Rinv(u,a)
     real,dimension(3,3) :: Rinv,Rn
@@ -115,14 +67,4 @@ contains
 
   end function Rcalc
 
-
-  function u_half(q,n_x)
-    integer :: n_x,i
-    real,dimension(n_x,3) :: q,u_half
-    ! Calculate u at half steps
-    do i=1,n_x-1
-      u_half(i,:) = (q(i,:) + q(i+1,:))/2.0
-    end do
-    u_half(n_x,:) = (q(1,:) + q(n_x,:))/2.0
-  end function u_half
 end module transform
