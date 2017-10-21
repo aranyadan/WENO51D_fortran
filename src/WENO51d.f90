6c132922fa77c12af1e06f6022bd7856c3bf4667
Aranya Dan <aranya.dan1996@gmail.com>
2017-10-21 18:02:30 +0530

    Added improvements

--- src/WENO51d.f90
+++ src/WENO51d.f90
@@ -4,25 +4,26 @@ subroutine WENO51d(lambda,F,q,dx,n_x,hp,hn)
   implicit none
   integer :: n_x,i,j
   real,dimension(n_x,3) :: F,q,hp,hn,Ftemp,qtemp
+  real,dimension(-1:n_x+3,3) :: Fnew,qnew
   real :: lambda,dx
   real,dimension(1) :: rho,u,E,p,a
-  real,dimension(6,3) :: F_i,q_i,g_i,v_i
+  real,dimension(6,3) :: F_i,q_i,g_i,v_i,F_i2,q_i2
   real,dimension(3,3) :: R_i,Rinv_i
   real,dimension(1,3) :: q_h,hpr,hnr
 
-  do i=1,n_x
-    do j = 6,1,-1
-      Ftemp = turn(F,n_x,3-j)
-      qtemp = turn(q,n_x,3-j)
-      F_i(j,:) = Ftemp(i,:)
-      q_i(j,:) = qtemp(i,:)
-    end do
+  Fnew(1:n_x,:) = F(1:n_x,:)
+  qnew(1:n_x,:) = q(1:n_x,:)
 
-    if(i == n_x) then
-      q_h(1,:) = (q(i,:) + q(1,:))/2.0
-    else
-      q_h(1,:) = (q(i,:) + q(i+1,:))/2.0
-    end if
+  qnew(-1,:) = qnew(1,:); qnew(0,:) = qnew(1,:)
+  qnew(n_x+1,:) = qnew(n_x,:); qnew(n_x+2,:) = qnew(n_x,:); qnew(n_x+3,:) = qnew(n_x,:)
+
+  Fnew(-1,:) = Fnew(1,:); Fnew(0,:) = Fnew(1,:)
+  Fnew(n_x+1,:) = Fnew(n_x,:); Fnew(n_x+2,:) = Fnew(n_x,:); Fnew(n_x+3,:) = Fnew(n_x,:)
+
+  do i=1,n_x
+    F_i(1:6,:) = Fnew(i-2:i+3,:)
+    q_i(1:6,:) = qnew(i-2:i+3,:)
+    q_h(1,:) = (qnew(i,:) + qnew(i+1,:))/2.0
 
     call primitives(q_h,1,rho,u,E,p,a)
     R_i = Rcalc(u(1),a(1))
